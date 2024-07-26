31c3629f6153f17edd0e844647acdc9d5d631e0f
Valera Shabanov <github@shasoft.com>
2024-07-26 18:10:24 +0300

    Исправление MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -9,14 +9,18 @@ import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserCaloriesPerDay;
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
 @Controller
 public class MealRestController {
-    protected final Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
     @Autowired
     private MealService service;
 
@@ -30,14 +34,32 @@ public class MealRestController {
         return MealsUtil.getTos(service.getAll(authUserId()), authUserCaloriesPerDay());
     }
 
+    public List<MealTo> getFilterAll(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+        log.info("getFilterAll");
+        List<MealTo> meals = this.getAll();
+        if (startDate != null) {
+            meals = meals.stream().filter(meal -> meal.getDateTime().toLocalDate().isAfter(startDate)).collect(Collectors.toList());
+        }
+        if (startTime != null) {
+            meals = meals.stream().filter(meal -> meal.getDateTime().toLocalTime().isAfter(startTime)).collect(Collectors.toList());
+        }
+        if (endDate != null) {
+            meals = meals.stream().filter(meal -> meal.getDateTime().toLocalDate().isBefore(endDate)).collect(Collectors.toList());
+        }
+        if (endTime != null) {
+            meals = meals.stream().filter(meal -> meal.getDateTime().toLocalTime().isBefore(endTime)).collect(Collectors.toList());
+        }
+        return meals;
+    }
+
     public void delete(int id) {
         log.info("delete {}", id);
         service.delete(id, authUserId());
     }
 
-    public void update(Meal meal) {
-        log.info("update {}", meal);
+    public void update(Meal meal, int id) {
+        log.info("update {} with id={}", meal, id);
+        assureIdConsistent(meal, id);
         service.update(meal, authUserId());
     }
-
 }

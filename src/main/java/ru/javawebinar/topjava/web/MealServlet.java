df12fe0db9440ac274e4880dd63e4a6035e41371
Valera Shabanov <github@shasoft.com>
2024-07-26 21:07:06 +0300

    remove SecurityUtil.authUserIdOrFail

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -29,7 +29,7 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
-        final int userId = SecurityUtil.authUserIdOrFail();
+        final int userId = SecurityUtil.authUserId();
         String id = request.getParameter("id");
         Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
                 LocalDateTime.parse(request.getParameter("dateTime")),
@@ -44,7 +44,7 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-        final int userId = SecurityUtil.authUserIdOrFail();
+        final int userId = SecurityUtil.authUserId();
         switch (action == null ? "all" : action) {
             case "delete":
                 int id = getId(request);
--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,21 +1,11 @@
 package ru.javawebinar.topjava.web;
 
-import ru.javawebinar.topjava.util.exception.NotFoundException;
-
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
     public static Integer authUserId() {
         return 1;
-    }
-
-    public static int authUserIdOrFail() {
-        final Integer userId = authUserId();
-        if (userId == null) {
-            throw new NotFoundException("User is not authorized");
-        }
-        return userId;
     }
 
     public static int authUserCaloriesPerDay() {

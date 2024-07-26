110e10f59f9d93628ec3e0b29b703b743a36d775
Valera Shabanov <github@shasoft.com>
2024-07-26 21:10:30 +0300

    add MealRestController.create

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserCaloriesPerDay;
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
@@ -23,6 +24,12 @@ public class MealRestController {
     private final Logger log = LoggerFactory.getLogger(getClass());
     @Autowired
     private MealService service;
+
+    public Meal create(Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        return service.create(meal, authUserId());
+    }
 
     public Meal get(int id) {
         log.info("get {}", id);

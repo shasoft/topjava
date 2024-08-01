0c916b137c21e973dd3850913069916325ebd797
Valera Shabanov <github@shasoft.com>
2024-08-01 16:21:16 +0300

    fix inserting database

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,22 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
        ('Admin', 'admin@gmail.com', 'admin'),
        ('Guest', 'guest@gmail.com', 'guest');
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2020-01-30 10:00', 'Завтрак', 500),
+       (100000, '2020-01-30 13:00', 'Обед', 1000),
+       (100000, '2020-01-30 20:00', 'Ужин', 500),
+       (100000, '2020-01-31 00:00', 'Еда на граничное значение', 100),
+       (100000, '2020-01-31 10:00', 'Завтрак', 1000),
+       (100000, '2020-01-31 13:00', 'Обед', 500),
+       (100000, '2020-01-31 19:00', 'Ужин', 410);
 
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),

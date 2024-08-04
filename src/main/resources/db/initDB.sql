748b36c8374d19a150a0288463418cd3af78e72e
Valera Shabanov <github@shasoft.com>
2024-08-04 13:52:10 +0300

    fix init database

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS user_role;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -20,13 +20,13 @@ CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 CREATE TABLE meals
 (
     id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    user_id     INTEGER                           NOT NULL,
-    description VARCHAR                           NOT NULL,
-    date_time   TIMESTAMP           DEFAULT now() NOT NULL,
-    calories    INTEGER                           NOT NULL,
+    user_id     INTEGER   NOT NULL,
+    description VARCHAR   NOT NULL,
+    date_time   TIMESTAMP NOT NULL,
+    calories    INTEGER   NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX meals_unique_datetime_idx ON meals (user_id, datetime);
+CREATE UNIQUE INDEX meals_unique_user_id_datetime_idx ON meals (user_id, date_time);
 
 CREATE TABLE user_role
 (

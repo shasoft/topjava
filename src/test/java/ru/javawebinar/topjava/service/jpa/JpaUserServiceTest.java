26aed4f091ac9f8f2e4afdaacdc48e25274c5161
Valera Shabanov <github@shasoft.com>
2024-09-07 15:22:10 +0300

    add package ru.javawebinar.topjava.service.jdbc add package ru.javawebinar.topjava.service.jpa

---
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.service.jpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.UserServiceTest;
+
+import static ru.javawebinar.topjava.Profiles.DATAJPA;
+
+@ActiveProfiles(DATAJPA)
+public class JpaUserServiceTest extends UserServiceTest {
+}

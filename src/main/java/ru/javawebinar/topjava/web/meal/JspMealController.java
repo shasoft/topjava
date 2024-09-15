c7e688cd3f35c7497e26a9cb6dd56b95818e3c38
Valera Shabanov <github@shasoft.com>
2024-09-15 20:53:47 +0300

    fix routing 2

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -56,7 +56,7 @@ public class JspMealController extends AbstractMealController {
     @GetMapping("/delete")
     public String delete(HttpServletRequest request) {
         super.delete(getId(request));
-        return "redirect:meals";
+        return "redirect:/meals";
     }
 
     @GetMapping("/filter")

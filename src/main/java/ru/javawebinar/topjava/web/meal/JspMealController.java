05c066853917b71d931a1c2d1f4636c38f757fe0
Valera Shabanov <github@shasoft.com>
2024-09-15 13:45:34 +0300

    add JspMealController

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,77 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.model.Meal;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+@Controller
+public class JspMealController extends AbstractMealController {
+    @GetMapping("/meals")
+    public String getAll(HttpServletRequest request) {
+        request.setAttribute("meals", super.getAll());
+        return "meals";
+    }
+
+    @PostMapping("/meals")
+    public String save(HttpServletRequest request) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        Integer id = getId(request);
+        if (id == null) {
+            super.create(meal);
+        } else {
+            super.update(meal, id);
+        }
+        return "redirect:meals";
+    }
+
+    @GetMapping("/create")
+    public String create(HttpServletRequest request) {
+        request.setAttribute("meal",
+                new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
+        return "mealForm";
+    }
+
+    @GetMapping("/update")
+    public String update(HttpServletRequest request) {
+        request.setAttribute("meal", super.get(getId(request)));
+        return "mealForm";
+    }
+
+    @GetMapping("/delete")
+    public String delete(HttpServletRequest request) {
+        super.delete(getId(request));
+        return "redirect:meals";
+    }
+
+    @GetMapping("/filter")
+    public String filter(HttpServletRequest request) {
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        request.setAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
+        return "meals";
+    }
+
+    private Integer getId(HttpServletRequest request) {
+        String id = request.getParameter("id");
+        if (id.isEmpty()) return null;
+        return Integer.parseInt(id);
+    }
+}
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -12,8 +12,7 @@
     <h3><a href="index.jsp">Home</a></h3>
     <hr/>
     <h2>Meals</h2>
-    <form method="get" action="meals">
-        <input type="hidden" name="action" value="filter">
+    <form method="get" action="filter">
         <dl>
             <dt>From Date (inclusive):</dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
@@ -33,7 +32,7 @@
         <button type="submit">Filter</button>
     </form>
     <hr/>
-    <a href="meals?action=create">Add Meal</a>
+    <a href="create">Add Meal</a>
     <br><br>
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
@@ -56,8 +55,8 @@
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><a href="meals?action=update&id=${meal.id}">Update</a></td>
-                <td><a href="meals?action=delete&id=${meal.id}">Delete</a></td>
+                <td><a href="update?id=${meal.id}">Update</a></td>
+                <td><a href="delete?id=${meal.id}">Delete</a></td>
             </tr>
         </c:forEach>
     </table>

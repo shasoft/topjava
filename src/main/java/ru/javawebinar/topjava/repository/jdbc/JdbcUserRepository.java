0f6dfbd4a3d38fa1d0f33e20c9151f259f29571a
Valera Shabanov <github@shasoft.com>
2024-09-16 21:42:05 +0300

    add roles in JdbcUserRepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -2,17 +2,22 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 @Transactional(readOnly = true)
@@ -50,6 +55,23 @@ public class JdbcUserRepository implements UserRepository {
                 """, parameterSource) == 0) {
             return null;
         }
+        // Удалить старые роли
+        jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", user.getId());
+        // Добавить новые роли
+        final List<Role> rolesList = List.copyOf(user.getRoles());
+        jdbcTemplate.batchUpdate("INSERT INTO user_roles (user_id, role) VALUES (?,?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, user.getId());
+                        ps.setString(2, rolesList.get(i).name());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return rolesList.size();
+                    }
+                });
         return user;
     }
 
@@ -62,6 +84,7 @@ public class JdbcUserRepository implements UserRepository {
     @Override
     public User get(int id) {
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        selectRolesForUsers(users);
         return DataAccessUtils.singleResult(users);
     }
 
@@ -69,11 +92,32 @@ public class JdbcUserRepository implements UserRepository {
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
+        selectRolesForUsers(users);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        List<User> users = jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        selectRolesForUsers(users);
+        return users;
+    }
+
+    private void selectRolesForUsers(List<User> users) {
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("ids", users.stream().map(user -> user.getId()).toList());
+        Map<Integer, Set<Role>> rolesForUser = namedParameterJdbcTemplate.query("SELECT * FROM user_role WHERE user_id IN (:ids)",
+                parameters,
+                rs -> {
+                    Map<Integer, Set<Role>> mapRoles = new HashMap<>();
+                    while (rs.next()) {
+                        Set<Role> roles = mapRoles.computeIfAbsent(rs.getInt("user_id"), id -> new HashSet<>());
+                        roles.add(Role.valueOf(rs.getString("role")));
+                    }
+                    return mapRoles;
+                }
+        );
+        users.forEach(user -> user.setRoles(rolesForUser.get(user.getId())));
     }
 }

e068a77747ad455769cbe497d334612ba7220e6d
Valera Shabanov <github@shasoft.com>
2024-10-06 16:56:26 +0300

    fix MealRestControllerTest

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -42,7 +42,6 @@ public class Meal extends AbstractBaseEntity {
     @Column(name = "calories", nullable = false)
     @Range(min = 10, max = 5000)
     @NotNull
-    @NotBlank
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
